1c5203d13bb039478d54225e7bcc5b9319d9412d
Robert Coulson <43724396+Coulsyltd@users.noreply.github.com>
2023-07-20 13:32:07 +0100

    SAme

--- views/partials/our-services.php
+++ views/partials/our-services.php
@@ -5,7 +5,7 @@
             <h4 class="fs-5"><i class="bi bi-door-open" style="font-size: 2rem; color: #18bc9c;"></i> <a href="<?= $service_inspectors_url ?>" class="">Fire Door Inspectors</a></h4>
             <p>The correct inspection of your fire door and its frame are cruicial to its performance.</p>
             <div class="d-flex justify-content-between">
-                <a href="<?= $service_inspectors_url ?>" class="">More...</a>
+                <a href="<?= $service_inspectors_url ?>" class="">Further information...</a>
                 <a href="areas-covered-fire-door-inspectors" class="small" style="text-decoration: none; color: lightgray;">Areas covered</a>
             </div>
             <hr>
@@ -14,7 +14,7 @@
             <h4 class="fs-5"><i class="bi bi-tools" style="font-size: 2rem; color: #18bc9c;"></i> <a href="<?= $service_installers_url ?>" class="">Fire Door Installers</a></h4>
             <p>The correct installation of fire door assemblies and doorsets is fundamental to their overall performance.<br>It will ensure the fire door will remain reliable to its fire integrity rating.</p>
             <div class="d-flex justify-content-between">
-                <a href="<?= $service_installers_url ?>" class="">More...</a>
+                <a href="<?= $service_installers_url ?>" class="">Further information...</a>
                 <a href="areas-covered-fire-door-installers" class="small" style="text-decoration: none; color: lightgray;">Areas covered</a>
             </div>
             <hr>
@@ -23,7 +23,7 @@
             <h4 class="fs-5"><i class="bi bi-door-closed" style="font-size: 2rem; color: #18bc9c;"></i> <a href="<?= $service_fd_maintenance_url ?>" class="">Fire Door Maintenance</a></h4>
             <p>We provide our clients with a proactive approach to fire door maintenance and complience.</p>
             <div class="d-flex justify-content-between">
-                <a href="<?= $service_fd_maintenance_url ?>" class="">More...</a>
+                <a href="<?= $service_fd_maintenance_url ?>" class="">Further information...</a>
                 <a href="areas-covered-fire-door-maintenance" class="small" style="text-decoration: none; color: lightgray;">Areas covered</a>
             </div>
             <hr>
@@ -33,7 +33,7 @@
             <p>Reactive and proactive property maintenance.</p>
             <p class="small">Please visit our Landlord Services page to see full details on what we can offer.</p>
             <div class="d-flex justify-content-between">
-                <a href="<?= $service_prop_maintenance_url ?>" class="">More...</a>
+                <a href="<?= $service_prop_maintenance_url ?>" class="">Further information...</a>
                 <a href="areas-covered-property-maintenance" class="small" style="text-decoration: none; color: lightgray;">Areas covered</a>
             </div>
             <hr>
